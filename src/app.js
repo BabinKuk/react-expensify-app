988065e31106ed4213c7f36703976cdce59993b9
babinkuk <nikola.babic@gmail.com>
2019-07-16 13:51:27 +0200

    remove dummy test data

--- src/app.js
+++ src/app.js
@@ -14,10 +14,10 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-// addExpenses
-store.dispatch(addExpense({ description: 'Racun Voda', amount: 300, createdAt: 300 }));
-store.dispatch(addExpense({ description: 'Racun Benzin', createdAt: -1000 }));
-store.dispatch(addExpense({ description: 'Racun Stanarina', amount: 150000 }));
+// addExpenses dummy test data
+//store.dispatch(addExpense({ description: 'Racun Voda', amount: 300, createdAt: 300 }));
+//store.dispatch(addExpense({ description: 'Racun Benzin', createdAt: -1000 }));
+//store.dispatch(addExpense({ description: 'Racun Stanarina', amount: 150000 }));
 
 // setFilterText
 // store.dispatch(setTextFilter('voda'));
@@ -30,9 +30,10 @@ store.dispatch(addExpense({ description: 'Racun Stanarina', amount: 150000 }));
 const state = store.getState();
 console.log('state', state);
 
+// dummy test data
 // visibleExpenses
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log('visibleExpenses', visibleExpenses);
+//const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//console.log('visibleExpenses', visibleExpenses);
 
 const jsx = (
     // provide store to all of the components
